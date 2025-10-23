6053a2f4bd1aec23dc77a2abdb34c10345da7823
andrekeys <andrekeys@boisestate.edu>
2025-10-23 10:13:35 -0600

    progress on aho-corasick

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/a-tk/go-algorithms
 
+require github.com/a-tk/go-datastructures latest
+
 go 1.24.5
--- strmatch/aho-corasick.go
+++ strmatch/aho-corasick.go
@@ -1,36 +1,69 @@
 package strmatch
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/a-tk/go-datastructures/graph"
+)
 
 // use {he, she, his, hers}
 // string x to be searched is ushers
 
 type Matcher[T string | []byte] struct {
-	g        func(state int, a T) (next int, fail bool)
-	f        func(state int) (prev int)
-	output   func(state int) (pattern T, fail bool)
-	patterns []T
+	g      *graph.GraphAutomata[int, byte]
+	f      map[int]int // from a state go to another state
+	output map[int][]T
 }
 
 func constructGoto[T string | []byte](patterns []T) (
-	g func(state int, a T) (next int, fail bool),
-	output func(state int) (pattern T, fail bool),
+	g *graph.GraphAutomata[int, byte],
+	output map[int][]T,
 ) {
+	g = graph.NewGraphAutomata[int, byte]()
+	output = make(map[int][]T)
+	newState := 0
 
+	enter := func(pattern T) {
+		state := 0
+		j := 0
+		// walk to a new state. For example if "he" already exists and we are adding "hers"
+		for _, ok := g.GetTransition(state, pattern[j]); ok; j++ {
+			state, ok = g.GetTransition(state, pattern[j]) // TODO: yuck
+		}
+		for p := j; p < len(pattern); p++ {
+			newState = newState + 1
+			g.AddTransition(state, newState, pattern[p])
+			state = newState
+		}
+		// add this to the output list
+		output[state] = append(output[state], pattern)
+	}
+	for i := 0; i < len(patterns); i++ {
+		enter(patterns[i])
+	}
+	// for all a such that g(0,a) == fail, do g(0, a) = 0
+	// this is basically saying for all letters that would fail to
+	//  transition from 0, make them instead transition to zero
+	// I think this is unnecessary, as the GraphAutomata will simply fail, and the state won't change
+
+	return
 }
 
 func NewMatcher[T string | []byte](patterns []T) *Matcher[T] {
-	return nil
+	g, output := constructGoto(patterns)
+	return &Matcher[T]{
+		g:      g,
+		output: output,
+	}
 }
 
 func (m *Matcher[T]) Match(x T) {
 	state := 0
 	for i := 0; i < len(x); i++ {
-		for _, fail := m.g(state, x[i]); fail; state = m.f(state) {
+		for _, fail := m.g.GetTransition(state, x[i]); fail; state = m.f[state] {
 		}
-		state, _ = m.g(state, x[i])
-		if p, matched := m.output(state); matched {
-			fmt.Printf("Match! %d: %s", i, p)
+		state, _ = m.g.GetTransition(state, x[i])
+		if p, matched := m.output[state]; matched {
+			fmt.Printf("Match! %d: %s", i, p) // TODO: p is actually a slice of strings
 		}
 	}
 }
